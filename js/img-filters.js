6a2ab8edf6ab1dac99c5906d61aa977d8234f8fa
Dmitry Vorobev <dm.siberian@gmail.com>
2021-07-10 11:59:38 +0300

    added JSDoc comments

--- js/img-filters.js
+++ js/img-filters.js
@@ -5,6 +5,11 @@ const defaultFilterButton = imgFiltersForm.querySelector('#filter-default');
 const randomFilterButton = imgFiltersForm.querySelector('#filter-random');
 const discussedFilterButton = imgFiltersForm.querySelector('#filter-discussed');
 
+/**
+ * Добавляет класс кнопке, по которой был произведен клик, удаляет этот класс с других кнопок.
+ *
+ * @param {Event} evt - событие 'click'
+ */
 const toggleButtonClass = function (evt) {
   for (const button of buttons) {
     button.classList.remove('img-filters__button--active');
@@ -12,6 +17,11 @@ const toggleButtonClass = function (evt) {
   evt.target.classList.add('img-filters__button--active');
 };
 
+/**
+ * Устанавливает фильтр 'По умолчанию' — фотографии в изначальном порядке с сервера.
+ *
+ * @param {Function} callback - функция отрисовки миниатюр
+ */
 const setFilterDefault = function (callback) {
   defaultFilterButton.addEventListener('click', (evt) => {
     toggleButtonClass(evt);
@@ -19,6 +29,11 @@ const setFilterDefault = function (callback) {
   });
 };
 
+/**
+ * Устанавливает фильтр 'Случайные' — заданное количество случайных, не повторяющихся фотографий.
+ *
+ * @param {Function} callback - функция отрисовки миниатюр
+ */
 const setFilterRandom = function (callback) {
   randomFilterButton.addEventListener('click', (evt) => {
     toggleButtonClass(evt);
@@ -26,6 +41,11 @@ const setFilterRandom = function (callback) {
   });
 };
 
+/**
+ * Устанавливает фильтр 'Обсуждаемые' — фотографии, отсортированные в порядке убывания количества комментариев.
+ *
+ * @param {Function} callback - функция отрисовки миниатюр
+ */
 const setFilterDiscussed = function (callback) {
   discussedFilterButton.addEventListener('click', (evt) => {
     toggleButtonClass(evt);
